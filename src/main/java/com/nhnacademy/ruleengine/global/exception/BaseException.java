56219cc1ddce23c8b38e977ab90aafdf26f485ee
{dabin5682} <dabin5682@gmail.com>
2026-07-15 09:29:19 +0900

    fix: globalException 오류수정

--- src/main/java/com/nhnacademy/ruleengine/global/exception/BaseException.java
+++ src/main/java/com/nhnacademy/ruleengine/global/exception/BaseException.java
@@ -1,7 +1,7 @@
 package com.nhnacademy.ruleengine.global.exception;
 
 import lombok.Getter;
-import okhttp3.internal.http2.ErrorCode;
+
 
 @Getter
 public class BaseException extends RuntimeException {
--- src/main/java/com/nhnacademy/ruleengine/global/exception/ErrorCode.java
+++ src/main/java/com/nhnacademy/ruleengine/global/exception/ErrorCode.java
@@ -4,15 +4,12 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@Getter
 @RequiredArgsConstructor
+@Getter
 public enum ErrorCode {
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "G001", "잘못된 입력값입니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "G002", "권한이 없습니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G003", "서버 오류가 발생했습니다."),
-    
-    // 회원 관련 에러 예시
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "존재하지 않는 회원입니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G003", "서버 오류가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
