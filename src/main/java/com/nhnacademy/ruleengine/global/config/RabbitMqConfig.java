d79226f467d927cf410106e78a66bfd403f92388
{dabin5682} <dabin5682@gmail.com>
2026-08-06 11:19:39 +0900

    refactor: import문 단순화

--- src/main/java/com/nhnacademy/ruleengine/global/config/RabbitMqConfig.java
+++ src/main/java/com/nhnacademy/ruleengine/global/config/RabbitMqConfig.java
@@ -1,12 +1,6 @@
 package com.nhnacademy.ruleengine.global.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Declarable;
-import org.springframework.amqp.core.Declarables;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
