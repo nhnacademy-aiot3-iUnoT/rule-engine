88438ba2443433c28d566126280cddb13373b7c8
{dabin5682} <dabin5682@gmail.com>
2026-08-10 17:05:43 +0900

    fix: API Path 수정

--- src/main/java/com/nhnacademy/ruleengine/engine/controller/SensorDataQueryController.java
+++ src/main/java/com/nhnacademy/ruleengine/engine/controller/SensorDataQueryController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/rule-engine")
+@RequestMapping("/api/core/rule-engine")
 public class SensorDataQueryController {
 
     private final SensorInfluxService sensorInfluxService;
