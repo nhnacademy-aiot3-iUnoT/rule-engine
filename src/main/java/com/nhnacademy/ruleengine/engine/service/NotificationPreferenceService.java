484127e95945bd5fb61f7c3de7a8a6aaddead4ed
Junwon Park <qkr13qkr@gmail.com>
2026-09-04 14:19:25 +0900

    NotificationPreferenceService DB연동

--- src/main/java/com/nhnacademy/ruleengine/engine/service/NotificationPreferenceService.java
+++ src/main/java/com/nhnacademy/ruleengine/engine/service/NotificationPreferenceService.java
@@ -2,23 +2,23 @@ package com.nhnacademy.ruleengine.engine.service;
 
 import com.nhnacademy.ruleengine.engine.dto.notification.NotificationChannel;
 import com.nhnacademy.ruleengine.engine.dto.notification.NotificationPreference;
+import com.nhnacademy.ruleengine.global.client.InventoryClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class NotificationPreferenceService {
+
+    private final InventoryClient inventoryClient;
+
     public List<NotificationPreference> findPreferences(
             Long organizationId,
             Long storageId,
             Long zoneId
     ) {
-        return List.of(
-                new NotificationPreference(1L, organizationId, storageId, zoneId, NotificationChannel.WEB, true, null),
-                new NotificationPreference(1L, organizationId, storageId, zoneId, NotificationChannel.TELEGRAM, true, "7501086554"),
-                new NotificationPreference(1L, organizationId, storageId, zoneId, NotificationChannel.KAKAO, false, "test-kakao-receiver-id")
-        );
+        return inventoryClient.findNotificationPreferences(organizationId, storageId, zoneId);
     }
-
-    //List<NotificationPreference> findByUserId(Long userId);
 }
